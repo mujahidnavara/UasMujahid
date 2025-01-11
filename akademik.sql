ad3adbff90bed188e4dc01543b97fa16750dea5b
mujahidnavara <zxstok0852@gmail.com>
2025-01-11 17:25:04 +0700

    akademik.sql

--- akademik.sql
+++ akademik.sql
@@ -1,21 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 13, 2022 at 03:35 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `akademik`
@@ -58,9 +46,6 @@ CREATE TABLE `mahasiswa` (
 --
 
 INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `alamat`, `fakultas`) VALUES
-(2, '2018420126', 'Gerald Alessandro Timothy', 'Surabaya', 'teknik'),
-(3, '2018420123', 'Adam', 'Solo', 'pertanian'),
-(4, '2018420120', 'Bagus Lenmarki Surya', 'Kediri', 'hukum');
 
 --
 -- Indexes for dumped tables
